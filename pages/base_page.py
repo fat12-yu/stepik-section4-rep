6194d97cf34e26dffedfe8935fe9b01bffae64bb
fat12-yu <fat12@mail.ru>
2019-12-02 10:14:57 +0400

    Добавили page object

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+
+
+    def open(self):
+        self.browser.get(self.url)
