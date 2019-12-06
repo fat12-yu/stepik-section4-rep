c2cc4efcfe85466d5645a0c15a0c5fbd92904673
fat12-yu <fat12@mail.ru>
2019-12-06 14:00:27 +0400

    Вынесение девелоперских настроек conftest.py в отдельный файл.

--- conftest.py
+++ conftest.py
@@ -1,21 +1,25 @@
 #!/usr/bin/python
 
-from selenium import webdriver
-import pytest
+
+try:
+    from conftest_dev import *
+except ImportError:
+    from selenium import webdriver
+    import pytest
 
 
-def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='en',
-                     help="Choose language")
+    def pytest_addoption(parser):
+        parser.addoption('--language', action='store', default='en',
+                         help="Choose language")
 
-@pytest.fixture(scope="function")
-def browser(request):
-    language = request.config.getoption('language')
-    print(f'\n######################## lang: {language}')
-    options = webdriver.ChromeOptions()
-    options.add_experimental_option('prefs', {'intl.accept_languages':language})
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome(options=options)
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
+    @pytest.fixture(scope="function")
+    def browser(request):
+        language = request.config.getoption('language')
+        print(f'\n######################## lang: {language}')
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('prefs', {'intl.accept_languages':language})
+        print("\nstart browser for test..")
+        browser = webdriver.Chrome(options=options)
+        yield browser
+        print("\nquit browser..")
+        browser.quit()
